fe143fed257a3cb3450f6a381fa1a9d3929145b9
Sihan Chen <39623753+Spycsh@users.noreply.github.com>
2024-11-05 21:10:06 +0800

    Create qwen2_vl.py

---
+++ qwen2_vl.py
@@ -0,0 +1,84 @@
+"""Note for enabling Qwen2-VL on HPU"""
+
+#from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
+from qwen_vl_utils import process_vision_info
+import torch
+import time
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--device", type=str, default="hpu")
+args = parser.parse_args()
+device = args.device
+
+links = ["https://raw.githubusercontent.com/opea-project/GenAIExamples/refs/heads/main/VisualQnA/ui/svelte/static/favicon.png","https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg","https://raw.githubusercontent.com/Ikaros-521/digital_human_video_player/refs/heads/main/static/imgs/1.png","https://raw.githubusercontent.com/Ikaros-521/digital_human_video_player/refs/heads/main/static/imgs/2.png","https://raw.githubusercontent.com/Spycsh/assets/refs/heads/main/OPEA%20Telemetry.jpg"]
+if device == "hpu":
+    from optimum.habana.transformers.modeling_utils import adapt_transformers_to_gaudi
+    adapt_transformers_to_gaudi()
+    #import habana_frameworks.torch.core as htcore
+    #import habana_frameworks.torch.gpu_migration
+# default: Load the model on the available device(s)
+from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
+model = Qwen2VLForConditionalGeneration.from_pretrained(
+    "Qwen/Qwen2-VL-2B-Instruct", torch_dtype="auto",
+).to(device)
+
+
+# We recommend enabling flash_attention_2 for better acceleration and memory saving, especially in multi-image and video scenarios.
+# model = Qwen2VLForConditionalGeneration.from_pretrained(
+#     "Qwen/Qwen2-VL-7B-Instruct",
+#     torch_dtype=torch.bfloat16,
+#     attn_implementation="flash_attention_2",
+#     device_map="auto",
+# )
+
+# default processer
+processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-2B-Instruct")
+
+# The default range for the number of visual tokens per image in the model is 4-16384. You can set min_pixels and max_pixels according to your needs, such as a token count range of 256-1280, to balance speed and memory usage.
+# min_pixels = 256*28*28
+# max_pixels = 1280*28*28
+# processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-7B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
+for link in links:
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image",
+                    "image": link,
+                },
+                {"type": "text", "text": "Describe this image."},
+            ],
+        }
+    ]
+
+    # Preparation for inference
+    text = processor.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True
+    )
+    image_inputs, video_inputs = process_vision_info(messages)
+    '''
+    inputs = processor(
+        text=[text],
+        images=image_inputs,
+        videos=video_inputs,
+        padding=True,
+        return_tensors="pt",
+    )
+    '''
+    inputs = processor(text=[text],images=image_inputs,videos=video_inputs,return_tensors="pt",padding='max_length',max_length=4096)
+    print(inputs['input_ids'].shape)
+    inputs = inputs.to(device)
+    # Inference: Generation of the output
+    for i in range(1):
+        start = time.time()
+        generated_ids = model.generate(**inputs, max_new_tokens=64,)
+        print(time.time() - start)
+    generated_ids_trimmed = [
+        out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+    ]
+    output_text = processor.batch_decode(
+        generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+    )
+    print(output_text)
